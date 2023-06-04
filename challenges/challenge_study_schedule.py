970db65b80e7cd4fd06862801bbd53c4d229aa35
eddev <edvaldojose003@gmail.com>
2023-06-04 15:58:05 -0300

    feat: Study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    result = 0
+
+    for x in permanence_period:
+        if type(x[0]) != int or type(x[1]) != int:
+            return None
+        if int(x[0]) <= target_time <= int(x[1]):
+            result += 1
+    return result
+# fim
