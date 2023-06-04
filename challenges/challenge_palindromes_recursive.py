18a9b58e615fcbdcf54891b8961d1ff7408c862c
Edvaldo José <edvaldojose003@gmail.com>
2023-06-04 19:44:40 -0300

    feat: palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if len(word) == 0:
+        return False
+    if low_index >= high_index:
+        return True
+
+    first_char = word[low_index]
+    last_char = word[high_index]
+
+    if first_char != last_char:
+        return False
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
