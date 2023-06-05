ceefa546923d0829d6e3c7984e7256ff3b67b5c6
Edvaldo José <edvaldojose003@gmail.com>
2023-06-04 22:28:44 -0300

    feat: correcao_challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    result = 0
-
-    for x in permanence_period:
-        if type(x[0]) != int or type(x[1]) != int:
+    count = 0
+    for x, j in permanence_period:
+        if not all(isinstance(value, int) for value in (x, j)) or x is None or j is None:
             return None
-        if int(x[0]) <= target_time <= int(x[1]):
-            result += 1
-    return result
+        if x <= target_time <= j:
+            count += 1
+    return count
