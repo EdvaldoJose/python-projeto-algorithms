694f25373c246474ecf3b798eb9a4a658b1a4b89
Edvaldo José <edvaldojose003@gmail.com>
2023-06-04 21:16:20 -0300

    feat: challenge_find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str):
+        return False
+
+    n = len(nums) - 1
+    count = [0] * (n + 1)
+
+    for num in nums:
+        if not isinstance(num, int) or num < 1 or num > n:
+            return False
+        count[num] += 1
+        if count[num] > 1:
+            return num
+
+    return False
